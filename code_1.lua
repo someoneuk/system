756fd26dc85391596528844fafb1d74b95be6f25
someoneuk <aryoyudhaalt@gmail.com>
2025-06-29 21:36:51 +0700

    Create code_1.lua

---
+++ code_1.lua
@@ -0,0 +1,13 @@
+-- [[
+ Script Detector by @tacotruckman01 or @NichunJelly
+you need to realize that digital footprint exist and others will always know your past doings
+so why can't you use that to do something good?
+digital footprints stays forever, keep that in mind
+have a nice day
+]]
+
+-- made this in mobile lol
+
+local result, fetch = loadstring(https://raw.githubusercontent.com/someoneuk/system/refs/heads/main/fetch_1)
+
+if result then
